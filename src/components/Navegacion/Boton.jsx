ca16e9a85e626bd08efc3e9e3bf9bc163125c19e
tsalinasv <101138315+tsalinasv@users.noreply.github.com>
2022-10-14 19:33:33 -0300

    Delete Boton.jsx

--- src/components/Navegacion/Boton.jsx
+++
@@ -1,9 +0,0 @@
-import Button from 'react-bootstrap/Button';
-
-function Boton() {
-  return (
-    <div>
-         <Button variant="outline-primary">Agregar al Carrito</Button>{' '}
-    </div>
-)}
-export default Boton;
