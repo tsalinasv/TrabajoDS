1ef6b53d58c565de216357974ad023358b7fec31
Tomás Salinas Venegas <tsalinasvenegas@gmail.com>
2022-10-29 00:35:53 -0300

    nueva info y fotos

--- src/components/Navegacion/Extra.jsx
+++ src/components/Navegacion/Extra.jsx
@@ -1,27 +1,79 @@
 import React from 'react'
-import Logo from "./Fotos/Logo.png";
+import { NavLink } from 'react-bootstrap';
+import FF from "./Fotos/FF.jpg";
+import MR from "./Fotos/MR.jpg";
+import Salud from "./Fotos/Salud.jpg";
+import Salud2 from "./Fotos/Salud2.png";
 
 function Extra() {
   return (
     <div>
         <div className='container'>
-            <h1 className="d-flex align-items-center justify-content-center" style={{fontWeight: 300, fontSize:43, marginTop:50, marginBottom:50}}>Blog Moda y Belleza</h1>
+            <h1 className="d-flex align-items-center justify-content-center" style={{fontWeight: 300, fontSize:43, marginTop:100, marginBottom:100}}>Blog Moda, Belleza y Emprendimiento</h1>
+            <h2 className="d-flex align-items-center justify-content-start" style={{fontWeight:500, marginBottom:50, marginTop:50}}>Nuevas Noticias de Interés:</h2>
             <div className='row'>
                 <div className='col-md-7 d-flex align-items-center justify-content-start'>
-                    <h2 style={{fontWeight:500, marginBottom:50, marginTop:50}}>Nuevas Noticias de Interés</h2>
+                    <div className='row'>
+                        <h3 style={{fontWeight: 300, fontSize:35, marginBottom:50}}>Fast Fashion: El corazón de la contaminación textil</h3>
+                        <p align="justify">
+                            Se le llama moda rápida o fast fashion al modelo de negocios que consiste en la producción masiva de prendas “desechables”, que genera un grave problema de contaminación. 
+                            Por ello, recientemente se anunció la incorporación de estos residuos en la ley de Reciclaje y Responsabilidad Extendida del Productor (REP). Sobre esta situación se refirieron 
+                            varios actores de esta cadena, Beatriz O`Brien, socióloga especializada en sustentabilidad textil y coordinadora nacional de la fundación internacional Fashion Revolution; 
+                            Guillermo González, jefe de la Oficina de Economía Circular del Ministerio del Medio Ambiente (MMA); Fernanda Kluever, gerenta de sostenibilidad de Tiendas Paris e Isidora Azolas 
+                            y Sofía Lillo, fundadoras de Pagana. La ropa ha acompañado a la humanidad a lo largo de toda su historia, como un reflejo de una identidad, cultura, forma de pensar y de sentir ... 
+                            La ropa evoluciona junto a las distintas generaciones, y para esta época, marcada por el cambio climático, los movimientos sociales y la información, se necesita una nueva tendencia ... 
+                        </p>
+                    </div>   
                 </div>
                 <div className='col-md-5 d-flex align-items-center justify-content-center'>
-                    <img src={Logo} height="100"/>
+                    <img src={FF} style={{borderRadius:25, marginTop:50, marginBottom:50}} height="250"/>
                 </div>
             </div>
+            <NavLink href="https://codexverde.cl/fast-fashion-el-corazon-de-la-contaminacion-textil/" className='d-flex align-items-center justify-content-center' style={{color:"#a65460", fontWeight:600, marginBottom:50}}>https://codexverde.cl/fast-fashion-el-corazon-de-la-contaminacion-textil/</NavLink>
             <div className='row'>
-                <div className='col-md-7 d-flex align-items-center justify-content-center'>
-                    <img src={Logo} height="100"/>
+                <div className='col-md-7 d-flex align-items-center justify-content-start'>
+                    <div className='row'>
+                        <h3 style={{fontWeight: 300, fontSize:35, marginBottom:50}}>La moda ‘retro’ vuelve para evitar el acoso a las mujeres, según un diario mexicano</h3>
+                        <p align="justify">
+                        El suplemento de moda del diario mexicano Reforma ha suscitado varias críticas por su artículo principal publicado este fin de semana. “El contexto de agresión o violencia contra las mujeres 
+                        llevó a las grandes firmas a apostar por prendas sobrias pero de ninguna manera aburridas o sin personalidad”, se lee en el llamado en portada de la edición de este sábado. Usuarios en Twitter 
+                        y Facebook retomaron la publicación acompañándola de críticas por el enfoque de esta casa editorial. Un par de fotografías en Facebook del ejemplar impreso se han compartido más de 850 veces 
+                        en las primeras 48 horas tras su publicación. El artículo que se presenta en la portada del suplemento fue escrito por Fernando Toledo y se titula “Discreto Glamour”. En el subtitular, el autor 
+                        hace referencia al movimiento #MeeToo y dice que a raíz de ellos varias firmas de alta costura están optando por atuendos discretos y que no permiten que se muestre mucha piel. “Los temas de conciencia 
+                        de género y acoso sexual, puestos en el ojo público por distintas iniciativas reivindicatorias, han derivado en que algunas firmas fashion dejen de lado los diseños sensuales”, se indica al inicio del artículo ...
+                        </p>
+                    </div>   
                 </div>
-                <div className='col-md-5 d-flex align-items-center justify-content-end'>
-                    <h2 style={{fontWeight:500, marginBottom:50, marginTop:50}}>Tu Salud Primero</h2>
+                <div className='col-md-5 d-flex align-items-center justify-content-center'>
+                    <img src={MR} style={{borderRadius:25, marginTop:50, marginBottom:50}} height="250"/>
                 </div>
             </div>
+            <NavLink href="https://verne.elpais.com/verne/2018/09/10/mexico/1536613366_326422.html" className='d-flex align-items-center justify-content-center' style={{color:"#a65460", fontWeight:600, marginBottom:50}}>https://verne.elpais.com/verne/2018/09/10/mexico/1536613366_326422.html</NavLink>
+            <h2 className='d-flex align-items-center justify-content-end' style={{fontWeight:500, marginBottom:50, marginTop:50}}>Tu Salud Primero:</h2>
+            <div className='row'>
+                <div className='col-md-5 d-flex align-items-center justify-content-center'>
+                    <div className='row'>
+                        <img src={Salud} style={{borderRadius:25, marginTop:50}} height="250"/>
+                        <img src={Salud2} style={{borderRadius:25, marginTop:50, marginBottom:50}} height="350"/>
+                    </div>
+                </div>
+                <div className='col-md-7 d-flex align-items-center justify-content-end'>
+                    <div className='row'>
+                        <h3 style={{fontWeight: 300, fontSize:35, marginBottom:50}}>La moda ‘retro’ vuelve para evitar el acoso a las mujeres, según un diario mexicano</h3>
+                        <p align="justify">
+                        Es grave la brecha existente en nuestro país entre las altas prevalencias de trastornos mentales entre la población —al alza durante la pandemia de COVID-19— y el escaso presupuesto público destinado a su 
+                        tratamiento y prevención. Frente a la proximidad de las elecciones presidenciales «y la oportunidad que brindan los cambios para identificar necesidades, repensar prioridades y definir posibles líneas de acción»
+                        , lo/as especialistas de esta columna para CIPER revisan la evidencia disponible al respecto hoy en Chile y sugieren líneas de mejora. Los trastornos mentales en Chile arrojan cifras alarmantes. Diversos estudios 
+                        muestran altas estadísticas al respecto entre la población, con la ansiedad y depresión como las de mayor índice, así como cifras considerables para consumo de alcohol y drogas. Estudios nacionales entre niños y 
+                        adolescentes (2017) señalan una prevalencia de trastornos mentales del 38% (cuatro de cada diez). La violencia intrafamiliar ha sido considerada una de las principales fuentes de trastornos mentales, y de acuerdo 
+                        a cifras entregadas por UNICEF, un 71% de los niños, niñas y adolescentes (NNA) chilenos recibe algún tipo de violencia ejercida por alguno de sus cuidadores primarios. La evidencia muestra que los NNA que experimentan 
+                        o son testigos de violencia, malos tratos o negligencia tienen dificultades para lograr los hitos del desarrollo esperados según su edad, así como un mayor riesgo de presentar trastornos conductuales, estrés postraumático, 
+                        ansiedad y depresión. Lo anterior a partir del impacto negativo de la adversidad y el trauma en la capacidad de regulación emocional, en las relaciones interpersonales, la memoria y la tolerancia al estrés ...
+                        </p>
+                    </div>   
+                </div>
+            </div>
+            <NavLink href="https://www.ciperchile.cl/2021/11/08/salud-mental-en-chile-urgencias-desafios-y-silencios/" className='d-flex align-items-center justify-content-center' style={{color:"#a65460", fontWeight:600, marginBottom:50}}>https://www.ciperchile.cl/2021/11/08/salud-mental-en-chile-urgencias-desafios-y-silencios/</NavLink>
             <h2 className="d-flex align-items-center justify-content-center"style={{fontWeight:500, marginBottom:50, marginTop:50}}>El Poder de las "Pymes"</h2>  
         </div>
     </div>
--- src/components/Navegacion/Testimonios.jsx
+++ src/components/Navegacion/Testimonios.jsx
@@ -18,19 +18,19 @@ function Testimonios() {
                 </div>
             </nav>
             <nav className='row'>
-                <div className='col-md-7 d-flex align-items-center justify-content-start' style={{marginTop:50, marginBottom:50}}>
+                <div className='col-md-8 d-flex align-items-center justify-content-start' style={{marginTop:50, marginBottom:50}}>
                     <nav className='row'>
                         <div className='caja col-md-5 d-flex align-items-start justify-content-start' style={{backgroundColor:"#a6546043", borderRadius:20, marginRight:20, marginTop:20}}>
                             <nav className='row' style={{margin:10}}>
                                 <h1 style={{fontWeight: 500, fontSize:85, color:"#a65460"}}>"</h1>
-                                <p style={{fontWeight:500}}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu</p>
+                                <p style={{fontWeight:500}}>"Me encantó! El servicio es extremadamente bueno, los productos llegaron rápido y tal como se veian en las fotos. Se lo recomendé a todas mis amigas."</p>
                                 <div className='col-md-3 d-flex align-items-center justify-content-start'>
                                     <img src={Like} height="40"/>
                                 </div>
                                 <div className='col-md-9 d-flex align-items-center justify-content-start'>
                                     <nav className='row'>
-                                        <p style={{fontWeight:800, fontSize:17, marginBottom:1, marginTop:15}}>Rocío Benavides</p>
-                                        <p style={{fontWeight:500, fontSize:12, marginTop:1}}>Dueña Fundadora</p>
+                                        <p style={{fontWeight:800, fontSize:17, marginBottom:1, marginTop:15}}>Andrea Venegas</p>
+                                        <p style={{fontWeight:500, fontSize:12, marginTop:1}}>Clienta</p>
                                     </nav>
                                 </div>
                             </nav>
@@ -38,7 +38,7 @@ function Testimonios() {
                         <div className='caja col-md-5 d-flex align-items-center justify-content-start' style={{backgroundColor:"#a6546043", borderRadius:20, marginTop:20}}>
                             <nav className='row' style={{margin:10}}>
                                 <h1 style={{fontWeight: 500, fontSize:85, color:"#a65460"}}>"</h1>
-                                <p style={{fontWeight:500}}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu</p>
+                                <p style={{fontWeight:500}}>"Comparar en mi propia tienda me permite tener la misma experiencia que cualquier clienta y como siempre, nuestro staff entrega un servicio increible."</p>
                                 <div className='col-md-3 d-flex align-items-center justify-content-start'>
                                     <img src={Like} height="40"/>
                                 </div>
@@ -82,7 +82,7 @@ function Testimonios() {
                         </div>
                     </nav>
                 </div>
-                <div className='col-md-5 d-flex align-items-center justify-content-start'>
+                <div className='col-md-4 d-flex align-items-center justify-content-center'>
                     <Form src="https://fgjgvtjwbhivqlw.form.io/sugerencias"/>
                 </div>
             </nav>
